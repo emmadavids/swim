0db2e3758a49b8c0047b7caeb409d61fcbf0e76e
emmadavids <davidsek88@gmail.com>
2022-07-29 21:01:38 +0100

    fklmdf

--- swim/urls.py
+++ swim/urls.py
@@ -24,7 +24,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    # path('', RedirectView.as_view(url='catalog/')),
+    path('', RedirectView.as_view(url='catalog/')),
     path('swim/', include('swim.urls')),
 
 
